f12e1540cbeb63bf5e01e8a6612501252669da9a
voze <victor@forsgrens.nu>
2016-08-27 15:54:02 +0200

    Fixing mah lifffe

--- StoneTable/Assets/Game.cs
+++ StoneTable/Assets/Game.cs
@@ -6,7 +6,10 @@ public class Game : MonoBehaviour {
     public Transform positionOFQue;
     public List<Transform> contentsOfQue = new List<Transform>();
     public List<Transform> allObjects = new List<Transform>();
-    public int height;
+    public float height;
+    public float currentSpeed = 10;
+    public float timer = 0;
+    public Transform itemInHand = null;
     // Use this for initialization
 	void Start () {
 	
@@ -14,12 +17,27 @@ public class Game : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        timer += Time.deltaTime;
         int current = 0;
         foreach(Transform t in contentsOfQue)
         {
             t.transform.position = new Vector3(positionOFQue.transform.position.x, positionOFQue.transform.position.y + (height * current));
 
             current++;
+        }
+        if (timer > currentSpeed)
+        {
+            timer = 0;
+            int x = Random.Range(0, allObjects.Count);
+            for (int y = 0; y < allObjects.Count; y++)
+            {
+                if (y == x)
+                {
+                    contentsOfQue.Add(allObjects.ToArray()[y]);
+                    Debug.Log("An new item has spawned and neeb tewbed everyone!");
+                    Instantiate(allObjects.ToArray()[y]);
+                }
+            }
         }
 	}
 }
---
+++ StoneTable/Assets/ItemScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemScript : MonoBehaviour {
+    public Transform GameHandler;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameHandler.GetComponent<Game>().itemInHand != null) {
+            if (GameHandler.GetComponent<Game>().itemInHand.Equals(transform))
+            {
+                transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            }
+        }
+	}
+    void OnMouseDown()
+    {
+        if (GameHandler.GetComponent<Game>().itemInHand != transform) {
+            GameHandler.GetComponent<Game>().itemInHand = transform;
+            GameHandler.GetComponent<Game>().contentsOfQue.Remove(transform);
+        }
+        else
+        {
+            GameHandler.GetComponent<Game>().itemInHand = null;
+            GameHandler.GetComponent<Game>().contentsOfQue.Add(transform);
+        }
+    }
+}
