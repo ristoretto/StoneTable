700cab50e0163a75f83681c7aacce12da12e1e9e
Christoffer Lundberg <ristoretto@gmail.com>
2016-08-28 17:59:04 +0200

    Last on local game jam

--- StoneTable/Assets/Game.cs
+++ StoneTable/Assets/Game.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Game : MonoBehaviour {
-
-    public GameObject Score;
+    public GameObject GameOver;
+    public AudioSource AudioReset;
+    public AudioSource AudioBuild;
+    public AudioSource AudioMaterial1;
+    public AudioSource AudioMaterial2;
+    public AudioSource AudioMaterial3;
+    public GameObject textTimer;
+    public GameObject textScore;
     public Transform positionOFQue;
     public List<GameObject> contentsOfQue = new List<GameObject>();
     public List<Transform> allObjects = new List<Transform>();
@@ -15,22 +22,36 @@ public class Game : MonoBehaviour {
     public float height;
     public int wrongs = 0;
     public int points = 0;
-    public float totalTimer = 15;
+    public float totalTimer = 24;
     public List<Sprite> imageList = new List<Sprite>();
     public int totalItemsSaved;
-    public float currentSpeed = 1;
+    public float currentSpeed = 0.1f;
     public float timer = 0;
+    private bool stopped = false;
     public Transform itemInHand = null;
     // Use this for initialization
 	void Start () {
         searchedForTypes.Add(ItemScript.itemType.Wood);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+    // Update is called once per frame
+    void Update () {
+
         if (totalTimer <= 0)
         {
-
+            if (!stopped) {
+                GameOver.SetActive(true);
+                //StoreHighscore(points);
+                AudioReset.Play();
+                foreach (GameObject o in contentsOfQue)
+                {
+                    Destroy(o);
+                }
+                contentsOfQue.Clear();
+                textScore.GetComponent<Text>().text = "age " + (100 + (int)points);
+                
+                stopped = true;
+            }
             return;
         }
         totalTimer -= Time.deltaTime;
@@ -60,7 +81,7 @@ public class Game : MonoBehaviour {
                         Transform tra2 = (Transform)tra;
                         tra2.SetParent(GameObject.Find("Canvas").transform);
                         tra2.transform.localScale = new Vector3(5.78f, 5.8f, 1);
-                        tra2.position = new Vector3(positionOFQue.transform.position.x, 2000, 0);
+                        tra2.position = new Vector3(positionOFQue.transform.position.x, 1725, 0);
                         contentsOfQue.Add(tra2.gameObject);
                         tra2.GetComponent<ItemScript>().setTravelThing(new Vector3(positionOFQue.transform.position.x, positionOFQue.transform.position.y + (128 * (contentsOfQue.Count - 1)), 0));
                     }
@@ -77,16 +98,34 @@ public class Game : MonoBehaviour {
         int x = Random.Range(0, 3);
         searchedForTypes.Clear();
         switch (x)
-            {
-                case 1:
-                    searchedForTypes.Add(ItemScript.itemType.Rock);
-                    break;
-                case 0:
-                searchedForTypes.Add(ItemScript.itemType.Wood);
-                    break;
+        {
+            case 1:
+                searchedForTypes.Add(ItemScript.itemType.Rock);
+                AudioMaterial1.Play();
+                break;
+
+            case 0:
+                //wood
+                switch (Random.Range(0, 3))
+                {
+                    case 1:
+                        searchedForTypes.Add(ItemScript.itemType.Wood);
+                        break;
+                    case 0:
+                        searchedForTypes.Add(ItemScript.itemType.Wood);
+                        break;
+                            case 2:
+                        searchedForTypes.Add(ItemScript.itemType.Wood);
+                        break;
+
+                }
+                AudioMaterial2.Play();
+                break;
+
                 case 2:
                 searchedForTypes.Add(ItemScript.itemType.Water);
-                    break;
+                AudioMaterial3.Play();
+                break;
             }
     
     }
@@ -94,12 +133,17 @@ public class Game : MonoBehaviour {
     {
         if (points >= 0)
         {
-            scoreImage.GetComponent<Image>().sprite = imageList.ToArray()[points / 4];
+            //AudioBuild.Play();
+            scoreImage.GetComponent<Image>().sprite = imageList.ToArray()[points];
         }
 
     }
     void updateScore() {
-        Score.GetComponent<Text>().text = "" + ((int)totalTimer);
+        textTimer.GetComponent<Text>().text = "" + ((int)totalTimer);
     }
 
+    public void PressReset() {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
--- StoneTable/Assets/ItemScript.cs
+++ StoneTable/Assets/ItemScript.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class ItemScript : MonoBehaviour {
     public Transform GameHandler;
     public Vector3 startMarker;
     public Vector3 endMarker;
-    private float speed = 500.0f;
+    private float speed = 2000.0f;
     private float startTime;
     private float journeyLength;
+
+    public Sprite wood1;
+    public Sprite wood2;
+    public Sprite wood3;
+
+    public Sprite rock1;
+    public Sprite rock2;
+    public Sprite rock3;
+
+    public Sprite water1;
+    public Sprite water2;
+    public Sprite water3;
+
     public enum itemType
     {
         Wood, Rock, Water
@@ -16,7 +29,9 @@ public class ItemScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         GameHandler = GameObject.Find("GameHandler").transform;
-	}
+        changeMaterialImage();
+
+    }
 	
     void Update()
     {
@@ -34,8 +49,8 @@ public class ItemScript : MonoBehaviour {
         journeyLength = Vector3.Distance(startMarker, endMarker);
     }
 
-	// Update is called once per frame
-	/*void Update () {
+    // Update is called once per frame
+    /*void Update () {
         if (GameHandler.GetComponent<Game>().itemInHand != null) {
             if (GameHandler.GetComponent<Game>().itemInHand.Equals(transform))
             {
@@ -55,4 +70,59 @@ public class ItemScript : MonoBehaviour {
             GameHandler.GetComponent<Game>().contentsOfQue.Add(transform);
         }
     }*/
+
+    void changeMaterialImage() {
+
+        if(type==itemType.Wood)
+        switch (Random.Range(0, 3))
+        {
+            case 1:
+                GetComponent<Image>().sprite = wood1;
+                break;
+            case 0:
+                    GetComponent<Image>().sprite = wood2;
+                    //searchedForTypes.Add(ItemScript.itemType.Wood);
+                    break;
+            case 2:
+                    GetComponent<Image>().sprite = wood3;
+                    //searchedForTypes.Add(ItemScript.itemType.Wood);
+
+                    break;
+        }
+
+        if (type == itemType.Rock)
+            switch (Random.Range(0, 3))
+            {
+                case 1:
+                    GetComponent<Image>().sprite = rock1;
+                    break;
+                case 0:
+                    GetComponent<Image>().sprite = rock2;
+                    //searchedForTypes.Add(ItemScript.itemType.Wood);
+                    break;
+                case 2:
+                    GetComponent<Image>().sprite = rock3;
+                    //searchedForTypes.Add(ItemScript.itemType.Wood);
+
+                    break;
+            }
+
+        if (type == itemType.Water)
+            switch (Random.Range(0, 3))
+            {
+                case 1:
+                    GetComponent<Image>().sprite = water1;
+                    break;
+                case 0:
+                    GetComponent<Image>().sprite = water2;
+                    //searchedForTypes.Add(ItemScript.itemType.Wood);
+                    break;
+                case 2:
+                    GetComponent<Image>().sprite = water3;
+                    //searchedForTypes.Add(ItemScript.itemType.Wood);
+
+                    break;
+            }
+
+    }
 }
